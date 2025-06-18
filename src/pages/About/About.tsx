32745f2b084c436d52837096dec3a1650ac8c841
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-06-18 15:55:07 -0500

    merging?

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <main className="about">
+      <h2>About</h2>
+      <h3>vite-react-ts-starter</h3>
+      <p>
+        🧑‍💻 Created by:{' '}
+        <a href="https://github.com/stefanbobrowski" target="_blank" rel="noopener noreferrer">
+          @stefanbobrowski
+        </a>
+      </p>
+      <p>
+        🧷 GitHub Code:{' '}
+        <a
+          href="https://github.com/stefanbobrowski/vite-react-ts-starter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          vite-react-ts-starter
+        </a>
+      </p>
+    </main>
+  );
+};
+
+export default About;
